97199feba8a97992a19a488b4c7e546b5005b0f7
Artyom-Mor0zov <efofab88@gmail.com>
2022-10-21 20:40:54 +0300

    Сделал с помощью Ромы

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите первое число: ");
+    int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+    int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine(numberA + numberB);
