8101fbcae49054a113c9ff9cf7ed86c3cb0d47a5
Artyom-Mor0zov <efofab88@gmail.com>
2022-10-22 16:20:35 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,6 @@
-﻿Console.Write("Введите первое число: ");
-    int numberA = int.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
-    int numberB = int.Parse(Console.ReadLine());
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
